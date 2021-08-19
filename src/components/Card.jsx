d9e57903c897a8c2176c0d32297742b6a6c64d42
serhio46 <1Serhio46@gmail.com>
2021-08-19 14:53:06 +0300

    fix: fixed image bug

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card({ obj }) {
 
 	return (
 		<div className="card">
-			<img height={90} width={90} src={`/img/${icon}`} alt="" />
+			<img height={90} width={90} src={`img/${icon}`} alt="" />
 			<div className="info">
 				<div>{title}</div>
 				<div className="value">{value}</div>
--- src/components/MainWeather.jsx
+++ src/components/MainWeather.jsx
@@ -6,7 +6,7 @@ function MainWeather({ main }) {
 		<div className="main">
 			<h2>{cityTitle}</h2>
 			<div className='overcast'>{clouds}</div>
-			<img width={250} height={250} src={`/img/${image}.svg`} alt="forecast" />
+			<img width={250} height={250} src={`img/${image}.svg`} alt="forecast" />
 			<div className='temp'>{temperature}&deg;С</div>
 			<div className='feels'>Feels like {feels}&deg;С</div>
 		</div>
